3b2cd201eeeb36c18a087bdebefe832ecd5af810
Agus Putradana A <agusamertha@gmail.com>
2020-06-27 19:25:36 +0700

    The code for the r programming course 3rd week

---
+++ cachematrix.R
@@ -0,0 +1,65 @@
+## A Pair of functions
+## that cache the inverse of a matrix
+
+## This function will create a special matrix object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+        ##Initialize the inverse property
+        i<-NULL
+        
+        ##Method to set the matrix
+        set<-function(matrix){
+                m<<-matrix
+                i<<-NULL
+        }
+        
+        ##Method to get the matrix
+        get<-function(){
+                ##Return the matrix
+                m
+        }
+        
+        ##Method to set the inverse of the matrix
+        setInverse<-function(inverse){
+                i<<-inverse
+        }
+        
+        ##Method to get the inverse of the matrix
+        getInverse<-function(){
+                ##Return the inverse property
+                i
+        }
+        
+        ##Return a list of the methods
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+}
+
+
+## This function will compute the inverse of the special matrix returned by makeCacheMatrix function
+## above. If the inverse has already been calculated (and the matrix has not
+## changed), then the cachesolve function should retreive the inverse from the cache
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m<-x$getInverse
+        
+        ##Only return the inverse if it's already set
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        
+        ##Get the matrix from our object
+        data<-x$get()
+        
+        ##Calculate the inverse using matrix multiplication
+        m<-solve(data)%*%data
+        
+        ##Set the inverse to the object
+        x$setInverse(m)
+        
+        ##Return the matrix
+        m
+}
